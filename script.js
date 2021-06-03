6b2548f8ab5902707eac9151c3d3bdd8b312cb20
Jamal Rizki <jamalbrizki@gmail.com>
2021-06-03 17:18:57 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// Navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle('change')
+});
+// End of Navbar
+
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons button i');
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+  if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+    video.style.opacity = '.7'
+  } else {
+    video.pause()
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  }
+}
+
+btn.addEventListener('click', () => {
+  playPause()
+})
+
+
+video.addEventListener('timeupdate', () => {
+  
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%` 
+  if(video.ended) {
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  } 
+})
+// End of Section 2 Video
+
+var swiper = new Swiper('', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 70,
+      stretch: 0,
+      depth: 50,
+      modifier: 1,
+      slideShadows: true,
+    }
+  });
+
+  var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
